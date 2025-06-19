66b83abad133ba7e3513f992f70e5be6526604d0
Dilhara Bandara <dilharabandara3@gmail.com>
2025-06-19 11:42:49 +0530

    Q1 is completed

--- src/Folder1/Stack.java
+++ src/Folder1/Stack.java
@@ -3,24 +3,37 @@ package Folder1;
 public class Stack {
     private int top;
     private int maxSize;
-    Stack(int size){
+    private char[] stackArray;
+
+    Stack(int size) {
         maxSize = size;
         top = -1;
     }
 
+    public boolean isEmpty() {
         return top == -1;
     }
 
+    public boolean isFull() {
+        return top == maxSize - 1;
     }
 
-    public void push(char x){
+    public void push(char x) {
+        if (isFull()) {
             System.out.println("Stack is Full");
-            stackArray[++top] = x;
+        } else {
+            stackArray[top++] = x;
+            System.out.println("Pushing " + x + " onto the stack.");
         }
     }
 
+    public char pop() {
+        if (isEmpty()) {
             System.out.println("STACK EMPTY");
+            return '\0'; // Return null character if stack is empty
+        } else {
+            System.out.println("Popping " + stackArray[top] + " from the stack.");
+            return stackArray[top--];
         }
-        return stackArray[top--];
     }
 }
